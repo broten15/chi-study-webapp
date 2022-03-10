40d4f55418f71223e2a43ab479cb17b8d13c062c
broten15 <brotensam@gmail.com>
2022-03-10 00:19:52 -0600

    added tab in data1

--- react-app/src/data/l1/l1data.js
+++ react-app/src/data/l1/l1data.js
@@ -137,114 +137,114 @@ export const l1trans = [
 ];
 
 export const l1sents = [
-`1. A對B有興趣
-A is interested in B
-我進了大學以後才知道自己對文學,歷史也很有興趣。 I didn't find out that I was also interested in literature and history until after I entered college.
-(1)
-我對歷史很有興趣。
-I'm very interested in history.
-(2)
-張先生對看電影有没有興趣? 
-Is Mr. Chang interested in watching movies?
-(3)
-他對文學沒有興趣。
-He is not interested in rature.
-(4) 我對這本書完全没有興趣。
-I have no interest in this book whatsoever.
-`,
+    `1. A對B有興趣
+    A is interested in B
+    我進了大學以後才知道自己對文學,歷史也很有興趣。 I didn't find out that I was also interested in literature and history until after I entered college.
+    (1)
+    我對歷史很有興趣。
+    I'm very interested in history.
+    (2)
+    張先生對看電影有没有興趣? 
+    Is Mr. Chang interested in watching movies?
+    (3)
+    他對文學沒有興趣。
+    He is not interested in rature.
+    (4) 我對這本書完全没有興趣。
+    I have no interest in this book whatsoever.
+    `,
 
-`2. 尤其
-especially, particularly
-這幾門課都相當重,尤其是化學課。 These several courses are quite difficult, especially chemistry.
-尤其 SV
-(1)
-他們家的人都很高,他父親尤其高。
-The people in his family are all very tall; his father is especially
-tall.
-(2)
-這儿的東西都很貴,吃的東西尤其貴。 Everything here is expensive; food is especially expensive.
-尤其 V
-(3) 
-我不喜歡吃中國菜,我尤其喜歡吃南方菜。 I don't like to eat Chinese food, I especially don't like to eat Southern (Chinese) food.
-(4)
-我喜歡看電影,我尤其喜歡看美國電影。 I like to watch movies; I especially like to watch American movies.`,
+    `2. 尤其
+    especially, particularly
+    這幾門課都相當重,尤其是化學課。 These several courses are quite difficult, especially chemistry.
+    尤其 SV
+    (1)
+    他們家的人都很高,他父親尤其高。
+    The people in his family are all very tall; his father is especially
+    tall.
+    (2)
+    這儿的東西都很貴,吃的東西尤其貴。 Everything here is expensive; food is especially expensive.
+    尤其 V
+    (3) 
+    我不喜歡吃中國菜,我尤其喜歡吃南方菜。 I don't like to eat Chinese food, I especially don't like to eat Southern (Chinese) food.
+    (4)
+    我喜歡看電影,我尤其喜歡看美國電影。 I like to watch movies; I especially like to watch American movies.`,
 
-`尤其 N
-(5)
-我喜歡吃中國菜,尤其是南方菜。
-I like to eat Chinese food, especially Southern food.
-(6)
-我討厭做家事,尤其是洗碗。
-I hate doing housework, especially washing dishes.`,
+    `尤其 N
+    (5)
+    我喜歡吃中國菜,尤其是南方菜。
+    I like to eat Chinese food, especially Southern food.
+    (6)
+    我討厭做家事,尤其是洗碗。
+    I hate doing housework, especially washing dishes.`,
 
-`3. Question Words + 都
-indicating inclusiveness
-她甚麼都好,就是男朋友的電話太多。
-Everything about her is good, it's just that her boyfriend calls too much.
-(1) 
-你在哪儿住都行。 Wherever you live is alright.
-(2)
-多麼貴的書我都買得起。
-No matter how expensive the books are, I can afford them.
-(3) 
-我怎麼做他都不高興。 However I do it, he's not happy.
-(4)
-張先生誰都喜歡。
-Everyone likes Mr. Chang. (or)Mr. Chang likes everyone. Note: The in this sentence can be either the subject or the object of the verb 喜歡.
-(5) 你甚麼時候來都可以。 Whatever time you come is fine.`,
+    `3. Question Words + 都
+    indicating inclusiveness
+    她甚麼都好,就是男朋友的電話太多。
+    Everything about her is good, it's just that her boyfriend calls too much.
+    (1) 
+    你在哪儿住都行。 Wherever you live is alright.
+    (2)
+    多麼貴的書我都買得起。
+    No matter how expensive the books are, I can afford them.
+    (3) 
+    我怎麼做他都不高興。 However I do it, he's not happy.
+    (4)
+    張先生誰都喜歡。
+    Everyone likes Mr. Chang. (or)Mr. Chang likes everyone. Note: The in this sentence can be either the subject or the object of the verb 喜歡.
+    (5) 你甚麼時候來都可以。 Whatever time you come is fine.`,
 
-`4.
-Question Words + 都...就是
-indicating inclusiveness
-她甚麼都好,就是男朋友的電話太多。 Everything about her is good, it's just that her boyfriend calls too much.
-(1) 
-你在哪儿住都行,就是別住在學校宿舍。 Wherever you five is alright, just don't livé in the school's dormitory.
-(2)
-誰都要去就是他不想去。
-Everyone wants to go, only he doesn't want to go.
-(3)
-我甚麼時候都有空,就是星期三没有空。 
-I'm free any (other) time, it's just Wednesday that I'm busy.
-(4) 
-這本書甚麼都好,就是太貴。 Everything about this book is good, it's just too expensive.`,
+    `4.
+    Question Words + 都...就是
+    indicating inclusiveness
+    她甚麼都好,就是男朋友的電話太多。 Everything about her is good, it's just that her boyfriend calls too much.
+    (1) 
+    你在哪儿住都行,就是別住在學校宿舍。 Wherever you five is alright, just don't livé in the school's dormitory.
+    (2)
+    誰都要去就是他不想去。
+    Everyone wants to go, only he doesn't want to go.
+    (3)
+    我甚麼時候都有空,就是星期三没有空。 
+    I'm free any (other) time, it's just Wednesday that I'm busy.
+    (4) 
+    這本書甚麼都好,就是太貴。 Everything about this book is good, it's just too expensive.`,
 
-`5.
-還没...呢
-still have not...
-有幾本書還沒買呢!
-There are several books (I) still haven't bought!
-(1)
-A: 你的書買了没有?
-A:Have you bought your books (yet)?
-B:我有好幾本書還沒買呢。 B:There are quite a few books I haven't bought yet.
-(2)
-A: 你早飯吃了没有? A:Have you eaten breakfast?
-B: 我還沒吃呢。 B: I still haven't eaten.
-(3)
-A: 你妹妹畢業了吧?
-A: I suppose your little sister has graduated?
-B: 她還沒畢業呢。
-B:She still hasn't graduated.`,
+    `5.
+    還没...呢
+    still have not...
+    有幾本書還沒買呢!
+    There are several books (I) still haven't bought!
+    (1)
+    A: 你的書買了没有?
+    A:Have you bought your books (yet)?
+    B:我有好幾本書還沒買呢。 B:There are quite a few books I haven't bought yet.
+    (2)
+    A: 你早飯吃了没有? A:Have you eaten breakfast?
+    B: 我還沒吃呢。 B: I still haven't eaten.
+    (3)
+    A: 你妹妹畢業了吧?
+    A: I suppose your little sister has graduated?
+    B: 她還沒畢業呢。
+    B:She still hasn't graduated.`,
 
-`還在V呢
-still V-ing
-(1)
-已經上午十一點了,他還在睡覺呢。 It's already eleven (o'clock), and he's still sleeping.
-(2)
-大家都吃完了,只有他一個人還在吃呢。 Everyone's done eating, only he is still eating.
-(3)
-他還在看書呢,真用功。 He's still studying. (He's) really diligent.`,
+    `還在V呢
+    still V-ing
+    (1)
+    已經上午十一點了,他還在睡覺呢。 It's already eleven (o'clock), and he's still sleeping.
+    (2)
+    大家都吃完了,只有他一個人還在吃呢。 Everyone's done eating, only he is still eating.
+    (3)
+    他還在看書呢,真用功。 He's still studying. (He's) really diligent.`,
 
-`6. 光是A就B
-only, merely, just
-光是物理課的書就花了一百五十多塊錢。 Just for the books for my physics class, I spent over one hundred and fifty dollars.
-(1)
-光是預備中文課就花了三個鐘頭。 (He) spent three hours just preparing for the Chinese lesson.
-(2)
-在美國上大學很貴。光是住宿舍就要兩千塊錢。
-Attending college in America is very expensive. It costs two
-thousand dollars alone to live in the dormitory.
-(3)
-整理屋子真麻煩,光是掃地我就覺得累得不得 了。 Cleaning house is really a pain. Just sweeping the floor tires me out.`,
+    `6. 光是A就B
+    only, merely, just
+    光是物理課的書就花了一百五十多塊錢。 Just for the books for my physics class, I spent over one hundred and fifty dollars.
+    (1)
+    光是預備中文課就花了三個鐘頭。 (He) spent three hours just preparing for the Chinese lesson.
+    (2)
+    在美國上大學很貴。光是住宿舍就要兩千塊錢。
+    Attending college in America is very expensive. It costs two
+    thousand dollars alone to live in the dormitory.
+    (3)
+    整理屋子真麻煩,光是掃地我就覺得累得不得 了。 Cleaning house is really a pain. Just sweeping the floor tires me out.`,
 
 ];
