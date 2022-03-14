3e68bccdbbb99910699b653ff5495ab766c86047
broten15 <brotensam@gmail.com>
2022-03-14 15:11:29 -0500

    added lesson 2 sentences

--- react-app/src/data/l2/l2data.js
+++ react-app/src/data/l2/l2data.js
@@ -118,5 +118,96 @@ export const l2py = [
 ];
 
 export const l2sents = [
+`1.
+居然
+surprisingly, unexpectedly
+你在小學的時候常說最討厭歷史課,現在居然也喜歡文學、歷史了。
+When you were in elementary school, you always said that you hated history. Now you surprisingly like literature and history.
+(1)
+我們已經二十多年沒見了,他居然還記得我的名 字。
+It's already been twenty years since we have seen each other, (but) surprisingly he still remembers my name. 
+(2)
+他在日本住了好幾年,沒想到居然一句日本話都不會說。 He lived in Japan for a number of years. It did not occur to me that he (surprisingly) would not be able to speak even one sentence of Japanese.`,
 
+`2.
+讓 
+to make; to cause
+這讓我很高興。
+This makes me very happy.
+(1)
+這件事讓我很高興。
+This matter makes me very happy.
+(2)
+弟弟不喜歡上學讓父母很生氣。
+My little brother's dislike of school makes my parents very angry.`,
+
+`3.
+為了
+in order to
+很多人為了畢業以後找工作,學一些自己並不喜歡的 課。 
+In order to find a job after graduation, many people take courses that they themselves don't like.
+(1) 
+為了學好中文,他跟中國人住在一塊儿。 
+In order to learn Chinese well, he lives with Chinese people.
+(2) 為了找一個好工作,他去念大學。 
+In order to find a good job, he went to college.
+...是為了
+(3)
+他跟中國人住在一塊儿是為了學好中文。 
+He lived with Chinese people in order to learn Chinese.
+(4) 
+他去念大學是為了找一個好工作。 
+He went to college in order to find a good job.
+為了 + noun phrase
+for the sake of N; because of N
+(5)
+為了生活,他一天到晚努力工作。
+For the sake of living, he works hard day and night.
+(6)
+為了工作,我可以不睡覺。
+For the sake of work, I don't have to sleep.`,
+
+`4. 
+A 對 B 有好處
+A is beneficial to B
+多選一些不同的課,對你是很有好處的。 
+Taking more different courses is beneficial to you.
+(1) 運動對身體有好處。 
+Exercise is beneficial to your health.
+(2) 
+喝酒對你没有好處 
+Drinking is not good for you.
+(3) 
+會說中文對找工作有没有好處? 
+Is knowing Chinese beneficial in finding a job?
+`,
+
+`5. 
+連...都 /也 ...
+even (intensive stressing the comparative degree)
+有時連週末都得上班。
+Sometimes I even have to work on weekends.
+連 + noun
+(1)
+媽媽連週末都得上班。 
+Mom even has to work on weekends.
+(2)
+他做的飯連狗都不吃。 
+Even dogs won't eat his cooking.
+(3)
+他連我說的話都不相信。 
+He won't even believe what I say.
+(4)
+他連明天的功課都做好了。
+He even finished tomorrow's homework.
+連 + verb construction
+(1)
+這麼小的屋子,連我一個人住都太小。
+This room is so small, even if I live alone, it's still too small.
+(2)
+他連我穿甚麼顏色的衣服都管。
+He even wants to control what color clothes I wear. 
+(3)
+他看報的時候連別人跟他說話都聽不見。
+When he reads the paper, he can't even hear other people speaking to him.`,
 ];
