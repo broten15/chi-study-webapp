bcb463e7652452f52cc8cf5e205a10d3b6b711d2
broten15 <brotensam@gmail.com>
2022-04-04 19:34:50 -0500

    added l4 and l5 sentences

--- react-app/src/data/l4/l4data.js
+++ react-app/src/data/l4/l4data.js
@@ -205,5 +205,125 @@ export const l4py = [
 ];
 
 export const l4sents = [
+`1.
+約
+to set up an appointment or date with someone
+昨天我跟他在電話裡約好了,今天三點來跟他面談。
+Yesterday I made an appointment with him by phone to come see him at three o'clock today.
+(1)
+我的明天去看電影。 
+I made a date with him to see a movie tomorrow.
+(2)
+他約我去打球。
+He made a date with me to play ball.
+(3)
+我跟他約好了,明天兩點在圖書館見面。
+I made an appointment with him. We will meet tomorrow at two o'clock in the library.
+(4)
+我跟牙醫約好了,今天下午去看牙。
+I made an appointment with the dentist for this afternoon to have a check-up.
+(5) 
+這幾天我很忙,要是你想來看我得先跟我約好。 
+These past few days I've been very busy. If you want to see me you must first make an appointment with me.`,
+
+`2. 
+一 ... 就 ...
+whenever; as soon as; once .... then ....
+電話一打完就請您進去。 
+As soon as he has finished calling, please go in.
+*Used with one subject
+(1) 
+我一吃涼的東西(我)就不舒服。 
+As soon as I eat cold things, I don't feel well.
+(2) 
+我一看書(我)就想睡覺。 
+As soon as I start reading, I want to sleep.
+(3) 
+(我)一想到這件事我就生氣。
+As soon as I think about this I get angry.
+(4)
+(我)一打電話我就緊張。 
+As soon as I call I get nervous.
+*Used with two different subjects
+(5)
+老師一生氣學生就不敢說話了。 
+As soon as the teacher gets angry, the students don't dare speak up.
+(6) 
+你一說我就懂了。 
+As soon as you said it I understood.
+(7) 
+我們一上車,他就唱起歌來了。 
+As soon as we got into the car, he started singing.`, 
+
+`3. 
+是 [來/去] V 的
+expresses purpose in coming or going
+我是來申請工作的。 
+I have come to apply for a job.
+(1)
+我不是來找王先生的,我是來找王太太的。 
+I have not come to look for Mr. Wang, I'm here to look for Mrs. Wang.
+(2) 
+你是不是到這儿來念書的? 
+Did you come here to study?
+(3) 我下個月去紐約,是去開會的不是去買東西的。 
+Next month I'm going to New York to attend a meeting, not to go shopping.`,
+
+`4.
+除了...以外,還/也...
+除了學習中文以外,還參加了一個旅遊團。 
+Aside from studying Chinese, I have also participated in a tour group.
+(1) 
+我除了會說中文以外還會說日文。 
+Besides being able to speak Chinese, I can also speak Japanese.
+(2) 
+這個學期除了物理以外我也選了化學跟歷史課。
+This semester, besides physics, I am also taking chemistry and history.
+(3)
+除了王先生以外,李先生也對文學有興趣。 
+Besides Mr. Wang, Mr. Li is also interested in literature.`,
+
+`5. 
+A, B, C, . . .這些(這 Num M) N
+Gives a series of examples of the topic in question. Word order is the opposite of English.
+我到過上海、蘇州、南京、北京這些地方。 
+I have been to these places: Shanghai, Soochow, Nanking and Peking.
+(1)
+他的皮包裡有筆、錢、眼鏡這些東西。
+Her purse has such things as pens, money and glasses in it.
+(2)
+像跑步、打球這些運動對身體都有好處。
+Exercises like running and playing ball are beneficial to your health.`,
+
+`6. 
+A對B有幫助
+A is helpful to B
+不知道這些經驗對你們的工作有沒有幫助。 
+I don't know if these experiences will be beneficial to your work.
+(1) 
+多吃水果對身體有幫助。 
+Eating more fruit is good for your health.
+(2) 
+這本書對你寫報告有没有幫助? 
+Is this book useful to you in writing your paper?
+(3)
+你的意見好是好,可是對這件事沒有甚麼幫助。
+Your opinion is good alright, but it's not much help in this matter.`,
+
+`7.
+在...之內
+within ...
+我們會在一星期之内把結果通知你的。
+We will notify you of the results within a week.
+(1)
+在一個星期之内我得把報告寫完。 
+I must finish writing my paper within a week.
+(2) 
+他在一個月之内能不能把那本書翻譯完? C
+an he finish translating that book within one month?
+(3)
+在三哩之内你找不到別的中國飯館。 
+You couldn't find another Chinese restaurant within three miles.`,
+
 
 ];
--- react-app/src/data/l5/l5data.js
+++ react-app/src/data/l5/l5data.js
@@ -190,5 +190,115 @@ export const l5py = [
 ];
 
 export const l5sents = [
+`1.
+忙著
+to be busy (doing something)
+有些人忙著找暑期工作。 
+Some people are busy looking for summer jobs.
+(1)
+快要期中考了,大家都忙著預備功課。 
+It will soon be mid-terms. Everyone is busy preparing for the exam.
+(2)
+A: 你這幾天在忙(些)甚麼?
+A: What have you been busy doing for the past few days?
+B:我忙著整理屋子。
+B: I'm busy straigtening up my room.
+(3)
+我每天忙著做功課。
+I am busy every day doing homework.`,
+
+`2. 
+到...為止 
+up to (a certain time or degree)
+到現在為止,我的成績都很好。 
+Up to now, my grades have been very good.
+(1)
+到明天為止,我在這兒已經住了兩年了。 
+By tomorrow, I will have already lived here for three years.
+(2)
+我們今天上課就到下午三點為止。
+We will have class until three o'clock today.
+(3)
+到現在為止,他還沒回來。 
+Up to now, he still has not returmed.
+(4)
+跑步就跑到那條街為止。 
+Jog up to that street.
+(5) 
+你們可以吃到飽為止。 
+You can eat until you are full.
+(6) 
+聽錄音帶得聽到能記住為止。 
+Listen to the tape until you have it memorized.`,
+
+`3.
+除了...以外
+except for
+除了化學課有幾個實驗做得不成功以外,其他幾門課大概都是A。
+I probably got an A in all of my classes except for chemistry, in which I had a few unsuccessful experiments.
+(1) 
+除了小張以外,別的人都是這個大學畢業的。 
+Except for Little Zhang, all the others graduated from this university.
+(2) 
+除了這本書,桌子上只有一枝筆。 
+Besides this book, only one pen is on the table.
+(3) 
+除了我以外没有人會說中文。 
+Besides me, no one else can speak Chinese. (Only I can speak Chinese.)`,
+
+`4.
+被 
+Passive voice marker
+有一個男同學的照相機被偷了。
+A male student's camera was stolen.
+(1)
+A: 魚被貓(給) 吃了。 
+A: The fish was eaten by the cat.
+B: 魚被吃了。 
+B: The fish was eaten.
+(2)
+A: 糖都被狗吃完了。 
+A: All the candy was eaten by the dog.
+B: 糖被吃完了。
+B: The candies were eaten.
+(3) 
+你的書被他(給)拿走了。 
+Your book was taken away by him.
+(4) 
+杯子被弟弟打破了。 
+The cup was broken by (his) younger brother.`,
+
+`5.
+一直 V 到 ... (為止)
+Indicates that an action continues up to a certain point.
+晚會一直開到早上三點。
+The party lasted until three o'clock in the morning.
+(1)
+他開會一直開到十二點。
+His meeting lasted until twelve o'clock.
+(2) 
+他每天念書一直念到把每一個生字都記住了為止。 
+Every day he studies until he has memorized every vocabulary item.`,
+
+`6. 
+A對B有影響
+A has an impact on B; A influences B
+這件事對我的生活有很大的影響。 
+This event has had a great impact on my life.
+(1)
+汽車對人的生活有很大的影響。
+Cars have a great influence on people's lives.
+= 汽車影響人的生活。 
+Cars influence people's lives.
+= 人的生活受汽車的影響。
+People's lives are influenced by cars.
+(2) 
+中文對日文有很大的影響。 
+The Chinese language has had a great influence on the Japanese language.
+= 中文影響了日文。
+The Chinese language has influenced the Japanese language.
+= 日文受到了中文的影響。 
+The Japanese language has been influenced by the Chinese language.`,
+
 
 ];
