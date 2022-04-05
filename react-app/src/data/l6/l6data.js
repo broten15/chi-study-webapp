e149b4e8d37da626ae01f04dd84a0e6d044076b0
broten15 <brotensam@gmail.com>
2022-04-04 19:41:47 -0500

    added lesson 6 setnences

--- react-app/src/data/l6/l6data.js
+++ react-app/src/data/l6/l6data.js
@@ -107,5 +107,95 @@ export const l6trans = [
 ];
 
 export const l6sents = [
+`1.
+又 ... 又 ...
+both ... and... ; not only ... but also...
+校園裡太危險了,又有偷竊又有強姦。 
+The campus is too dangerous, There are robberies as well as rapes.
+(1) 
+學校裡又有讀書的地方,又有運動的地方。 
+There are both study and exercising areas in the school.
+(2) 
+我又餓又累,得休息休息吃點儿東西才行。 
+I am both hungry and tired. I must rest and eat something.
+(3)
+這個學校,老師又好,學生又聰明,所以很有名。
+At this school the teachers are good and the students smart. Therefore it is very famous.
+(4) 
+小狗又跑又跳,高興得不得了。 
+The puppy is both running and jumping. He is extremely happy.
+(5) 
+他又會唱歌,又會跳舞。 
+He can both sing and dance.`,
+
+`2. 
+自從...以後...
+ever since...
+自從你上了大學以後,一年最多在家一個月。 
+Ever since you went to college, you are home for only one month per year at most.
+(1) 
+自從1979年以後,我一直住在美國。 
+From 1979 on, I have continuously lived in the USA.
+(2) 
+我自從學了中文以後,認識的中國人越來越多 了。 
+Ever since I learned Chinese, I have met more and more Chinese (people)`,
+
+`3.
+不是...就是
+if (it's) not ..., then (it's) ...
+兩個人整天混在一起,不是喝酒,就是跳舞。 
+The two of them hang around all day together. If they are not drinking, then they are dancing.
+(1)
+世界上只有兩種人,不是男的就是女的。 
+There are only two kinds of people in this world. If you are not male, then you're female.
+(2) 
+我每天不是念書就是工作,生活真没有意思。 
+Everyday, if I'm not studying, then I'm working. Life is simply not interesting.`,
+
+`4.
+只要 ... 就 ...
+as long as ... then ...
+男女只要相愛就可以生活在一起。 
+As long as a man and woman love each other, then they can live together.
+(1)
+這本書只要你喜歡我就給你。
+As long as you like this book I'll give it to you.
+(2)
+只要你肯請我吃飯我就幫你的忙。
+As long as you are willing to treat me to a meal, I'll help you.
+(3)
+在美國,只要有錢就可以買你喜歡的東西。 
+In America, as long as you have money, you can buy anything you like.
+只要 A not A 又有甚麼關係呢? 
+As long as ... what does it matter whether or not A?
+(1) 
+房子只要住得舒服,漂亮不漂亮又有甚麼關係呢? 
+As long as the house is comfortable, what does it matter whether it's beautiful or not?
+(2) 朋友只要性情好,有錢沒有錢又有甚麼關係呢? 
+As long as a friend's personality is good, what does it matter whether he has money or not?`,
+
+`5.
+才
+only; just
+他現在才高三,就想跟女朋友同居。 
+He's only in his third year of high school and already wants to live with his girlfriend.
+(1)
+這本書才三塊鑽,真便宜!
+This book is only three dollars. How cheap!!
+(2) 
+他今年才十五歲,怎麼能上大學呢? 
+This year he is only 15. How can he attend college?`,
+
+`6.
+用 ... 来 ...
+use ... to (for the purpose of) ...
+用甚麼來維持生活?
+What do you use to maintain your lifestyle?
+(1) 
+政府用一家一個孩子的人口政策來控制人口。 
+The government employs the one-family-one-child policy to control population.
+(2) 
+老師用考試的法子來逼學生念書。 
+The teacher uses examinations as a method to force students to study.`,
 
 ];
